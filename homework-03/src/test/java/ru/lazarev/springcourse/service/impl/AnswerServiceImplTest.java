969196ddc22edb52d75ab339fff10ecdc1917f18
vvlazarev <vvlazarev@mts.ru>
2023-08-14 18:01:50 +0300

    Homework 3: fix tests

--- homework-03/src/test/java/ru/lazarev/springcourse/service/impl/AnswerServiceImplTest.java
+++ homework-03/src/test/java/ru/lazarev/springcourse/service/impl/AnswerServiceImplTest.java
@@ -14,9 +14,9 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
