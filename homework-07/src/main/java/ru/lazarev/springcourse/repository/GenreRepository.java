140e0d9fd69c15d7dacfafbf80ea627c8e1a7f20
vvlazarev <vvlazarev@mts.ru>
2023-09-23 14:16:26 +0300

    Homework 7: post rewiev

--- homework-07/src/main/java/ru/lazarev/springcourse/repository/GenreRepository.java
+++ homework-07/src/main/java/ru/lazarev/springcourse/repository/GenreRepository.java
@@ -4,10 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import ru.lazarev.springcourse.domain.Genre;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface GenreRepository extends CrudRepository<Genre, Long> {
     List<Genre> findAll();
-
-    Optional<Genre> findById(Long id);
 }
