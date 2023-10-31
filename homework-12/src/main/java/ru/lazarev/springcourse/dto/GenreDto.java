9955e621a55e7000f0668b6f1c69b7159f943628
vvlazarev <vvlazarev@mts.ru>
2023-10-31 20:50:26 +0300

    Homework 12: init commit

---
+++ homework-12/src/main/java/ru/lazarev/springcourse/dto/GenreDto.java
@@ -0,0 +1,18 @@
+package ru.lazarev.springcourse.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class GenreDto {
+
+    Long id;
+
+    String name;
+}
