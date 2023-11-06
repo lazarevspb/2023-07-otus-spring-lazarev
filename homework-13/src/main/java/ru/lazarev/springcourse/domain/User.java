f572cabae1003ddfe04a5a3e7d5a3858af769350
vvlazarev <vvlazarev@mts.ru>
2023-11-06 20:21:26 +0300

    Homework 13

--- homework-13/src/main/java/ru/lazarev/springcourse/domain/User.java
+++ homework-13/src/main/java/ru/lazarev/springcourse/domain/User.java
@@ -9,10 +9,15 @@ import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -37,4 +42,9 @@ public class User {
     @Column
     private String password;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
 }
