917e9ec47b24ae2c5f058ecea24a0ae41732adf1
vvlazarev <vvlazarev@mts.ru>
2023-11-06 21:49:55 +0300

    Homework 13: fix tests

--- homework-13/src/test/java/ru/lazarev/springcourse/controller/SecurityControllerTest.java
+++ homework-13/src/test/java/ru/lazarev/springcourse/controller/SecurityControllerTest.java
@@ -1,39 +1,114 @@
 package ru.lazarev.springcourse.controller;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.lazarev.springcourse.mapper.BookMapperImpl;
+import ru.lazarev.springcourse.dto.BookDto;
+import ru.lazarev.springcourse.mapper.BookMapper;
 import ru.lazarev.springcourse.repository.BookRepository;
+import ru.lazarev.springcourse.repository.UserRepository;
+import ru.lazarev.springcourse.security.SecurityConfig;
 import ru.lazarev.springcourse.service.AuthorService;
 import ru.lazarev.springcourse.service.BookService;
 import ru.lazarev.springcourse.service.GenreService;
-import ru.lazarev.springcourse.service.impl.BookServiceImpl;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(BookController.class)
-@Import({BookController.class, BookMapperImpl.class, BookServiceImpl.class})
+@Import(SecurityConfig.class)
 public class SecurityControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
     @MockBean
     BookRepository repository;
     @MockBean
     AuthorService authorService;
     @MockBean
+    UserRepository userRepository;
+    @MockBean
+    BookService bookService;
+
+    @MockBean
+    BookMapper bookMapper;
+
+    @MockBean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager;
+    @MockBean
     GenreService genreService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void should_return_ok_when_books_with_role_admin() throws Exception {
+        mockMvc.perform(get("/books"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"UNKNOWN"})
+    public void should_return_forbidden_when_books_with_role_unknown() throws Exception {
+        mockMvc.perform(get("/books"))
+            .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"USER"})
+    public void should_return_ok_when_books_with_role_user() throws Exception {
+        mockMvc.perform(get("/books"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"USER"})
+    public void should_return_forbidden_when_books_edit_with_role_user() throws Exception {
+        mockMvc.perform(get("/books/edit?id=1"))
+            .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"ADMIN"})
+    public void should_return_ok_when_books_edit_with_role_admin() throws Exception {
+        when(bookMapper.map(any())).thenReturn(getBookDto());
+        mockMvc.perform(get("/books/edit?id=1"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"ADMIN"})
+    public void should_return_ok_when_books_new_with_role_admin() throws Exception {
+        mockMvc.perform(get("/books/new"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"USER"})
+    public void should_return_forbidden_when_books_new_with_role_user() throws Exception {
+        mockMvc.perform(get("/books/new"))
+            .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"USER"})
+    public void should_return_forbidden_when_books_save_with_role_user() throws Exception {
+        mockMvc.perform(post("/books/save"))
+            .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"ADMIN"})
+    public void should_return_ok_when_books_save_with_role_admin() throws Exception {
+        mockMvc.perform(post("/books/save"))
+            .andExpect(status().is3xxRedirection());
+    }
 
     @Test
     public void should_login_page_must_be_available_without_authentication() throws Exception {
@@ -48,33 +123,12 @@ public class SecurityControllerTest {
             .andExpect(status().isOk());
     }
 
-    @Test
-    public void should_get_unauthorized_status_if_unauthorized_request_on_books_page() throws Exception {
-        mockMvc.perform(get("/books"))
-            .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    public void should_get_forbidden_status_if_unauthorized_request_on_edit_page() throws Exception {
-        mockMvc.perform(post("/books/edit?id=2"))
-            .andExpect(status().isForbidden());
-    }
-
-    @Test
-    public void should_get_unauthorized_status_if_unauthorized_request_on_new_page() throws Exception {
-        mockMvc.perform(get("/books/new"))
-            .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    public void should_get_forbidden_status_if_unauthorized_request_on_save_page() throws Exception {
-        mockMvc.perform(post("/books/save"))
-            .andExpect(status().isForbidden());
-    }
-
-    @Test
-    public void should_get_forbidden_status_if_unauthorized_request_on_delete_page() throws Exception {
-        mockMvc.perform(delete("/books/5"))
-            .andExpect(status().isForbidden());
+    private BookDto getBookDto() {
+        var book = new BookDto();
+        book.setId(1L);
+        book.setTitle("title");
+        book.setAuthor("author");
+        book.setGenre("genre");
+        return book;
     }
 }
