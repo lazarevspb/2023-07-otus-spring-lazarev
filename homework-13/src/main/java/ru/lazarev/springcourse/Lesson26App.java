e31733755a4fd1eb406c13593b1381bfc75a7987
vvlazarev <vvlazarev@mts.ru>
2023-11-06 17:29:54 +0300

    Homework 13: init commit

---
+++ homework-13/src/main/java/ru/lazarev/springcourse/Lesson26App.java
@@ -0,0 +1,12 @@
+package ru.lazarev.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson26App {
+//    http://localhost:8080/books admin@1234
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson26App.class, args);
+    }
+}
