86115ccc4c6966226b909473f580a09d279ea889
vvlazarev <vvlazarev@mts.ru>
2023-12-17 19:22:58 +0300

    Homework 17

--- homework-17/src/main/java/ru/lazarev/springcourse/Lesson33App.java
+++ homework-17/src/main/java/ru/lazarev/springcourse/Lesson33App.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lesson33App {
 //    http://localhost:8080/books admin@1234
+//    http://localhost:8080/actuator/metrics
+//    http://localhost:8080/actuator/health
     public static void main(String[] args) {
         SpringApplication.run(Lesson33App.class, args);
     }
